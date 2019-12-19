b7a5e9a211131d4ab85c47cd28cde3ae989c725e
nymisha-vaddi <58929608+nymisha-vaddi@users.noreply.github.com>
2019-12-19 12:00:11 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+//forward labelling
+#include <stdio.h>
+int main()
+{
+    printf("\n gitam university");
+    goto a;
+    printf("\n dhyanahitha professional studies");
+    printf("\n promizeit services pvt ldt");
+    a:
+    printf("\n hyderabad");
+    return 0;
+}
