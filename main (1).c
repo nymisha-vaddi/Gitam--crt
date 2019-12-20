bada9026fff514441dc849d2d6a87086ba5d0293
nymisha-vaddi <58929608+nymisha-vaddi@users.noreply.github.com>
2019-12-20 10:41:55 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a=10,b=10;
+    printf("%d  ",a);
+    printf("%d  ",++a);
+    printf("%d  ",a);
+    printf("\n%d  ",b);
+    printf("%d  ",b++);
+    printf("%d  ",b);
+
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("\nsizeof(12)=%d",sizeof(12));
+    printf("\nsizeof('b')=%d",sizeof('b'));
+    printf("\nsizeof(12.6)=%d",sizeof(12.6));
+    printf("\nsizeof(12.6f)=%d",sizeof(12.6f));
+
+    return 0;
+}
