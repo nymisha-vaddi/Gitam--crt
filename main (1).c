0ba59d8eaabad5c91d0960f67ce70ae016d06dd4
nymisha-vaddi <58929608+nymisha-vaddi@users.noreply.github.com>
2019-12-20 09:39:07 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int x=200,y=200;
+    if((x-=100)||(y-=100))
+    printf("x=%d and y=%d",x,y);
+    else
+    printf("x=%d and y=%d",x,y);
+
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int x=100,y=200;
+    if((x-=100)&&(y-=100))
+    printf("x=%d and y=%d",x,y);
+    else
+    printf("x=%d and y=%d",x,y);
+    return 0;
+}
